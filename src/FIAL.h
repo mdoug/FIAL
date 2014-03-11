5152b8585c504135b8a08424d94338304bdaf803
Malcolm Douglas <malcolm.douglas@gmail.com>
2014-03-11 12:57:20 -0400

    Lost FIAL.h, rewriting
    Not sure what happened to this, must have been living in the /package directory.
    Anyhow, it's back.

---
+++ src/FIAL.h
@@ -0,0 +1,13 @@
+#ifndef FIAL_H
+#define FIAL_H
+
+#include "ash.h"
+#include "ast_defines.h"
+#include "basic_types.h"
+#include "api.h"
+#include "interp.h"
+#include "loader.h"
+#include "libs.h"
+#include "text_buf.h"
+
+#endif /*FIAL_H*/
