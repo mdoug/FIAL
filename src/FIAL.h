66017bf3600b2e88834c634e24ad18b059fb8a4f
Malcolm <malcolm@thelonious.monk>
2014-03-15 15:30:16 -0400

    Got "system" working for FreeBSD
    Now the build system won't work for windows, but it is  a quick fix,
    "system.c" is windows, "sys_bsd.c" is FreeBSD.

--- src/FIAL.h
+++ src/FIAL.h
@@ -1,13 +1,19 @@
 #ifndef FIAL_H
 #define FIAL_H
 
-#include "ash.h"
-#include "ast_defines.h"
+#include "ast.h"
 #include "basic_types.h"
 #include "api.h"
 #include "interp.h"
 #include "loader.h"
 #include "libs.h"
 #include "text_buf.h"
+
+
+/* this stuff is not permanent, but I'm putting it in here anyway for
+   now. */
+#include "value_def_short.h"
+#include "ast_def_short.h"
+#include "error_def_short.h"
 
 #endif /*FIAL_H*/
